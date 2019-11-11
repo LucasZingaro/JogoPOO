9b852616c133469d59bf2c29b5b66ded0b331369
Lucas Zingaro <37312287+LucasZingaro@users.noreply.github.com>
2019-11-11 17:07:54 -0200

    Up FRM

--- src/jogo/visao/FrmCarregarJogo.java
+++ src/jogo/visao/FrmCarregarJogo.java
@@ -9,12 +9,12 @@ package jogo.visao;
  *
  * @author aluno
  */
-public class FrmStart extends javax.swing.JFrame {
+public class FrmCarregarJogo extends javax.swing.JFrame {
 
     /**
-     * Creates new form FrmStart
+     * Creates new form FrmCarregarJogo
      */
-    public FrmStart() {
+    public FrmCarregarJogo() {
         initComponents();
     }
 
@@ -60,20 +60,20 @@ public class FrmStart extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrmStart.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmCarregarJogo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrmStart.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmCarregarJogo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrmStart.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmCarregarJogo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrmStart.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmCarregarJogo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FrmStart().setVisible(true);
+                new FrmCarregarJogo().setVisible(true);
             }
         });
     }
--- src/jogo/visao/FrmIniciarComNome.java
+++ src/jogo/visao/FrmIniciarComNome.java
@@ -16,12 +16,12 @@ import javax.swing.SwingConstants;
  *
  * @author aluno
  */
-public class Login extends javax.swing.JFrame {
+public class FrmIniciarComNome extends javax.swing.JFrame {
 
     /**
      * Creates new form Login
      */
-    public Login() {
+    public FrmIniciarComNome() {
         initComponents();
         for (Component myComponent : getComponents()) {
             if (myComponent instanceof JLabel) {
@@ -96,7 +96,7 @@ public class Login extends javax.swing.JFrame {
 
         lblNome.setText("Nome");
 
-        btnLogin.setText("Login");
+        btnLogin.setText("Iniciar");
 
         lblImg.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jogo/img/imgLogin.png"))); // NOI18N
         lblImg.setFocusable(false);
@@ -169,20 +169,23 @@ public class Login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmIniciarComNome.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmIniciarComNome.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmIniciarComNome.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmIniciarComNome.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Login().setVisible(true);
+                new FrmIniciarComNome().setVisible(true);
             }
         });
     }
